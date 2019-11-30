ca501096d47c8bbda639d7b78cfa5ac9fc2ab820
guplem <guplem@gmail.com>
2019-11-30 18:30:32 +0100

    Basic door script

---
+++ Musikote/Assets/Scripts/Door.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Interactable
+{
+
+    private bool isOpen = false;
+    
+    
+    public new bool Use()
+    {
+        if (!base.Use()) return false;
+
+        if (isOpen)
+            return Close();
+        else
+            return Open();
+    }
+    
+    public bool Open()
+    {
+        if (!base.Use()) return false;
+
+
+        return true;
+    }
+    
+    public bool Close()
+    {
+        if (!base.Use()) return false;
+
+
+        return true;
+    }
+
+}
