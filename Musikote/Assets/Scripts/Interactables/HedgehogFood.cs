93c149cde960aec85117da06a2c638e5c715511e
Dejavi77 <javierdelaserna7@gmail.com>
2019-12-02 00:29:51 +0100

    all interactables created

---
+++ Musikote/Assets/Scripts/Interactables/HedgehogFood.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HedgehogFood : Interactable
+{
+    protected new void Update()
+    {
+        base.Update();
+    }
+}
+    
---
+++ Musikote/Assets/Scripts/Interactables/Wash.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wash : Interactable
+{
+    [SerializeField] private Objetive ObjectiveToComplete;
+    
+    public override bool Use()
+    {
+        if (!base.Use()) return false;
+        GameManager.instance.CompleteObjective(ObjectiveToComplete);
+        return true;
+    }
+}
